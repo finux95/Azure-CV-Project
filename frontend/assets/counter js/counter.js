d78cec3488882e30e5f5da013364b6505fafcb35
finux95 <107047830+finux95@users.noreply.github.com>
2024-04-07 13:05:50 +0100

    Counter css moved to assets/css. Counter.js created and mvoed to assets/counter js. Indexlight.html updated with counter code and links to counter.css & counter.js

---
+++ frontend/assets/counter js/counter.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded',(event) =>{
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(Response => {
+        return Response.json()
+    }).then(Response =>{
+        console.log("website called function API.");
+        count = Response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
+//<script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+ 
+//$(document).ready(function(){
+//    $('.counter-value').each(function(){
+//        $(this).prop('Counter',0).animate({
+//            Counter: $(this).text()
+//        },{
+//            duration: 3500,
+//            easing: 'swing',
+//            step: function (now){
+//                $(this).text(Math.ceil(now));
+//            }
+//        });
+//    });
+//});
+
